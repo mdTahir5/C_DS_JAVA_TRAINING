e05d5ea883c9e5ba02ad16fcbe83e398bb3efbfb
mdTahir5 <mdtahir252550@gmail.com>
2024-07-22 15:31:08 +0530

    AddTwoNumberofDifferentDataType

---
+++ AddTwoNumberWithDifferentDataType.java
@@ -0,0 +1,7 @@
+public class AddTwoNumberWithDifferentDataType {
+    public static void main(String[] args) {
+        int a = 5;
+        float b = 7.5f;
+        System.out.println("Sum is : " + (a+b));
+    }
+}
