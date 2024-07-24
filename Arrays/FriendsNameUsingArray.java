2589777aafb47e5f518b068b8a7b99044c9fa9cb
mdTahir5 <mdtahir252550@gmail.com>
2024-07-24 22:01:04 +0530

    FriendsNameUsingArray

---
+++ Arrays/FriendsNameUsingArray.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class FriendsNameUsingArray {
+    public static void main(String[] args) {
+        String[] friendNames = {"Fahad", "Faraz" , "Faisal" , "Zaid" , "Altamash"};
+        // for each loop
+        for (String Name : friendNames) {
+            System.out.println(Name);
+        }
+    }
+}
