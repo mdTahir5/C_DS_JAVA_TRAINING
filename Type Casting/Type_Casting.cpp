73e191a9bf30d36b4b4e7eefc9bab37d8dfc837f
mdTahir5 <mdtahir252550@gmail.com>
2024-07-24 22:36:43 +0530

    Type Casting

---
+++ Type Casting/Type_Casting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int a = 5;
+    int b = 7;
+    float c = (float)(a + b)/5;
+    cout<<c;
+    return 0;
+}
